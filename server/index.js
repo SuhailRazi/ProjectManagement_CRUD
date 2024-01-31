ee1844ac7b1e8df2d79074ea8a5c320ce8fd663e
suhail <suhail.r@hubspire.com>
2024-01-31 20:49:03 +0530

    Project and client queries backend

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema.js");
+const connectDB = require("./config/db.js");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+connectDB();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
